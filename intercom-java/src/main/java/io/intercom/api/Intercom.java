34895f042934c258ba95b8d66f88d7f85908c06b
christianekstrand <christianekstrand@gmail.com>
2014-12-04 10:02:00 +0100

    Upped version

--- intercom-java/src/main/java/io/intercom/api/Intercom.java
+++ intercom-java/src/main/java/io/intercom/api/Intercom.java
@@ -14,7 +14,7 @@ public class Intercom {
 
     static final String AUTH_BEARER = "Bearer";
 
-    private static final String VERSION = "1.0.4-YouCruit";
+    private static final String VERSION = "1.0.5-YouCruit";
 
     public static final String USER_AGENT = "intercom-java/" + Intercom.VERSION;
 
